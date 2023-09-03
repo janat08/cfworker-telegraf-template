096b7ec0ce084f70c4977c1cb38783e1146ca423
janat08 <jey.and.key@gmail.com>
2023-09-04 02:39:20 +0600

    asdf

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const bot = new Telegraf(self.BOT_TOKEN);
 // Your code here, but do not `bot.launch()`
 // Do not forget to set environment variables BOT_TOKEN and SECRET_PATH on your worker
 
+bot.start((ctx) => ctx.reply(`
+Hi, this bot is for buying vanity emails.`));
+bot.help((ctx) => ctx.reply('email jey.and.key@gmail.com'));
+
+bot.command('buy', (ctx) => ctx.reply('Hello'));
+
 const router = new Router();
 router.post(`/${self.SECRET_PATH}`, createTelegrafMiddleware(bot));
 new Application().use(router.middleware).listen();
